badecf61d00d903910cbccfa09c84c5f05fb254f
fbenti <benti97@hotmail.it>
2021-04-09 15:24:54 +0200

    virtual added to create abstract class

--- frontier.h
+++ frontier.h
@@ -8,12 +8,12 @@ using namespace std;
 
 class Frontier {
   public:
-    void add(State state);
-    State pop();
-    bool is_empty();
-    int size();
-    bool contains(State state);
-    string getName();
+    virtual void add(State state) = 0;
+    virtual State pop() = 0;
+    virtual bool is_empty() = 0;
+    virtual int size() = 0;
+    virtual bool contains(State state) = 0;
+    virtual string getName() = 0;
 };
 
 

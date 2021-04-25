d1d512ad550781edb1738c2938417108bb937434
Vicente Bartual <vicentebartualf@gmail.com>
2021-04-25 14:19:12 +0200

    Bdi agent state

---
+++ bdi_agent_state.cpp
@@ -0,0 +1,219 @@
+#include <iostream>
+#include <cstdio>
+#include <random>
+#include <typeinfo>
+
+#include "action.h"
+#include "color.h"
+#include "bdi_agent_state.h"
+
+using namespace std;
+
+vector<vector<bool>> AgentState::walls;
+vector<int> AgentState::agent_colors;
+vector<int> AgentState::box_colors;
+
+AgentState::AgentState(int agent_id, int agent_row, int agent_col,
+		vector<vector<char>> boxes, vector<vector<char>> goal) {
+    this->agent_id = agent_id;
+    this->agent_row = agent_row;
+    this->agent_col = agent_col;
+    this->boxes = boxes;
+	this->goal = goal;
+	this->g = 0;
+}
+
+bool AgentState::operator==(const AgentState &other) const {
+  return this->agent_row == other.agent_row
+         && this->agent_col == other.agent_col
+         && this->boxes == other.boxes
+		 && this->goal == other.goal;
+}
+
+vector<AgentState*> AgentState::get_expanded_states() {
+    vector<AgentState*> expanded_states;
+	for (int act = 0; act < actions.size(); act++) {
+		Action action = actions[act];
+		if (this->is_applicable(action)) {
+			expanded_states.push_back(this->apply_action(action));
+		}
+	}
+	return expanded_states;
+}
+
+bool AgentState::is_applicable(Action action) {
+    if (action.type == ActionType::NOOP) {
+      return true;
+    }
+
+    else if (action.type == ActionType::MOVE) {
+      int destination_row = this->agent_row + action.ard;
+      int destination_col = this->agent_col + action.acd;
+      return is_free(destination_row, destination_col);
+    }
+
+    else if (action.type == ActionType::PUSH) {
+      int box_row = this->agent_row + action.ard;
+      int box_col = this->agent_col + action.acd;
+      char box = this->boxes[box_row][box_col];
+
+      // Check if there is a box in the given direction,
+      // and if the box belongs to this agent (same color)
+      if (box == ' ') {
+        return false;
+      }
+      int box_color = this->box_colors[int(box) - int('A')];
+      if (box_color != this->agent_color) {
+        return false;
+      }
+      // Check if the future box cell is occupied
+      // The agent will occupy the current box cell
+      int box_dst_row = box_row + action.brd;
+      int box_dst_col = box_col + action.bcd;
+      return is_free(box_dst_row, box_dst_col);
+    }
+
+    else if (action.type == ActionType::PULL) {
+      int box_row = this->agent_row - action.brd;
+      int box_col = this->agent_col - action.bcd;
+      char box = this->boxes[box_row][box_col];
+
+      // Check if there is a box in the given direction,
+      // and if the box belongs to this agent (same color)
+      if (box == ' ') {
+        return false;
+      }
+      int box_color = this->box_colors[int(box) - int('A')];
+      if (box_color != this->agent_color) {
+        return false;
+      }
+      // Check if the future box cell is occupied
+      // The agent will occupy the current box cell
+      int agent_dst_row = this->agent_row + action.ard;
+      int agent_dst_col = this->agent_col + action.acd;
+      return is_free(agent_dst_row, agent_dst_col);
+    }
+    return false;
+}
+
+AgentState* AgentState::apply_action(Action action) {
+    int copy_agent_row = this->agent_row;
+    int copy_agent_col = this->agent_col;
+    vector<vector<char>> copy_boxes(this->boxes);
+
+	if (action.type == ActionType::MOVE) {
+		copy_agent_row += action.ard;
+		copy_agent_col += action.acd;
+	} else if (action.type == ActionType::PUSH) {
+		copy_agent_row += action.ard;
+		copy_agent_col += action.acd;
+		// Box current and future position
+		int box_row = this->agent_row + action.ard;
+		int box_col = this->agent_col + action.acd;
+		int box_dst_row = box_row + action.brd;
+		int box_dst_col = box_col + action.bcd;
+		// Copy box to new cell, the delete it from the old one
+		copy_boxes[box_dst_row][box_dst_col] = copy_boxes[box_row][box_col];
+		copy_boxes[box_row][box_col] = ' ';
+	} else if (action.type == ActionType::PULL) {
+		copy_agent_row += action.ard;
+		copy_agent_col += action.acd;
+		// Box current and future position
+		int box_row = this->agent_row - action.ard;
+		int box_col = this->agent_col - action.acd;
+		int box_dst_row = box_row + action.brd;
+		int box_dst_col = box_col + action.bcd;
+		// Copy box to new cell, the delete it from the old one
+		copy_boxes[box_dst_row][box_dst_col] = copy_boxes[box_row][box_col];
+		copy_boxes[box_row][box_col] = ' ';
+	}
+
+	AgentState* copy_state;
+	copy_state = new AgentState(this->agent_id, copy_agent_row, copy_agent_col,
+		copy_boxes, this->goal);
+    copy_state->parent = this;
+    copy_state->action = action;
+    copy_state->g = this->g + 1;
+    return  copy_state;
+}
+
+
+bool AgentState::is_goal_state() {
+	char goal;
+	for (int row = 0; row < this->goal.size(); row++) {
+		for (int col = 0; col < this->goal[row].size(); col++) {
+			goal = this->goal[row][col];
+			if ('A' <= goal && goal <= 'Z'
+					&& this->boxes[row][col] != goal) {
+				return false;
+			} else if ('0' <= goal && goal <= '9'
+					&& goal == (char)(this->agent_id) + '0'
+					&& (this->agent_row != row && this->agent_col != col)) {
+				return false;
+			}
+		}
+	}
+	return true;
+};
+
+bool AgentState::is_free(int row, int col) {
+	return (!this->walls[row][col]
+			&& this->boxes[row][col] == ' '
+			&& (row != this->agent_row || col != this->agent_col));
+}
+
+vector<Action> AgentState::extract_plan() {
+    vector<Action> plan(this->g);
+    AgentState* state = this;
+    while (state->parent != nullptr) {
+		plan[state->g - 1] = state->action;
+		state = state->parent;
+    }
+    return plan;
+};
+
+int AgentState::hashCode() const {
+	int prime = 31;
+	int result = 1;
+	for (int row = 0; row < this->boxes.size(); ++row) {
+		for (int col = 0; col < this->boxes[row].size(); ++col) {
+			char c = this->boxes[row][col];
+			if (c!=0) {
+				result = prime * result + (row * this->boxes[row].size() + col) * c;
+			}
+		}
+	}
+	result = prime * result + this->agent_row * this->boxes[0].size()
+		+ this->agent_col;
+	return result;
+};
+
+string AgentState::repr()
+{
+    string lines;
+    for (int row = 0; row < this->boxes.size(); row++)
+    {
+      string line;
+      for (int col = 0; col < this->boxes[row].size(); col++)
+      {
+        if (this->boxes[row][col] != ' ')
+        {
+          line += this->boxes[row][col];
+        }
+        else if (this->walls[row][col])
+        {
+          line += "+";
+        }
+        else if (this->agent_row == row && this->agent_col == col)
+        {
+          line += (char)(this->agent_id) + '0';
+        }
+        else
+        {
+          line += " ";
+        }
+      }
+      lines = lines + line + "\n";
+    }
+    return lines;
+};
---
+++ bdi_agent_state.h
@@ -0,0 +1,61 @@
+#ifndef __BDI_AGENT_STATE_H
+#define __BDI_AGENT_STATE_H
+
+#include <vector>
+#include <string>
+#include "action.h"
+
+using namespace std;
+
+class AgentState {
+  public:
+	int agent_id;
+	int agent_row;
+	int agent_col;
+	int agent_color;
+    vector<vector<char>> boxes;
+    vector<vector<char>> goal;
+    static vector<vector<bool>> walls;
+    static vector<int> agent_colors;
+    static vector<int> box_colors;
+    Action action;
+    AgentState *parent;
+    int g; // depth
+
+    AgentState(int agent_id, int agent_row, int agent_col,
+		vector<vector<char>> boxes, vector<vector<char>> goal);
+
+    vector<AgentState*> get_expanded_states();
+
+    bool is_applicable(Action action);
+
+    AgentState* apply_action(Action action);
+
+    bool is_goal_state();
+
+    bool is_free(int row, int col);
+
+    vector<Action> extract_plan();
+
+    int hashCode() const;
+
+    bool operator==(const AgentState &other) const;
+
+    string repr();
+};
+
+class HashHelper3 {
+	public:
+		int operator()(const AgentState* state) const {
+			return state->hashCode();
+		}
+};
+
+class EqualHelper3 {
+	public:
+		int operator()(const AgentState* state1, const AgentState* state2) const {
+			return *state1 == *state2;
+		}
+};
+
+#endif
